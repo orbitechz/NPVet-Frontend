14e44685b10ab06a4c52893f0c4eb33b55757f12
Bouchra Akl <bushraakl1234@gmail.com>
2023-10-30 19:50:49 -0300

    feat : table listar usuarios

--- src/app/views/usuario/usuario-list/usuario-list.component.ts
+++ src/app/views/usuario/usuario-list/usuario-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Header } from 'src/app/components/table/header';
 import { Usuario } from 'src/app/models/usuario/usuario';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
@@ -8,28 +9,46 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
   styleUrls: ['./usuario-list.component.scss']
 })
 export class UsuarioListComponent implements OnInit {
-  data: Usuario[] = [];
   filteredData: Usuario[] = [];
+  @Input() isModal = false
+  isErro!: boolean
+  mensagem!: string
+  title = 'Tutores';
+  usuarios: Usuario[] = [];
+  data: any[] = [];
 
-  constructor(private usuarioService: UsuarioService) {}
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
     this.usuarioService.getAll().subscribe((response: Usuario[]) => {
       this.data = response;
-      this.filteredData = [...response]; 
+      this.filteredData = [...response];
     });
   }
 
   filterData(searchTerm: string): void {
     if (!searchTerm.trim()) {
-      this.filteredData = [...this.data]; 
+      this.filteredData = [...this.data];
     } else {
       this.filteredData = this.data.filter(usuario => {
         return (
           usuario.nome.toLowerCase().includes(searchTerm.toLowerCase()) ||
           usuario.cpf.includes(searchTerm) ||
-          usuario.username.toLowerCase().includes(searchTerm.toLowerCase())        );
+          usuario.username.toLowerCase().includes(searchTerm.toLowerCase()));
       });
     }
+  }
+
+  apiUrlPath() {
+    return 'http://localhost:8080/usuario';
+  }
+
+  callHeaders() {
+    let tableHeaders: Header[] = [];
+    tableHeaders.push(new Header('Nome', 'nome'));
+    tableHeaders.push(new Header('CPF', 'cpf'));
+    tableHeaders.push(new Header('Username', 'username'));
+    tableHeaders.push(new Header('Tipo de Usuário', 'tipoUsuario'));
+    return tableHeaders;
   }
 }
